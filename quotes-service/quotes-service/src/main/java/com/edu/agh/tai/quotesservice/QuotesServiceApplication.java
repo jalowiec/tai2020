7d0b9b0d243ba35103575bba951c508a22a11b88
unknown <lukasz.jalowiecki@gmail.com>
2020-10-25 19:08:54 +0100

    connect quote service with eureka

--- quotes-service/quotes-service/src/main/java/com/edu/agh/tai/quotesservice/QuotesServiceApplication.java
+++ quotes-service/quotes-service/src/main/java/com/edu/agh/tai/quotesservice/QuotesServiceApplication.java
@@ -2,8 +2,11 @@ package com.edu.agh.tai.quotesservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 //TODO testy jednostkowe
 @SpringBootApplication
+@EnableDiscoveryClient
 public class QuotesServiceApplication {
 
 	public static void main(String[] args) {
