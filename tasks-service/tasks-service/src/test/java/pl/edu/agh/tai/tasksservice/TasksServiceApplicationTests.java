3352cb41e0e76a947d28c3af1e19eace9d18d0d5
unknown <lukasz.jalowiecki@gmail.com>
2020-11-12 22:26:50 +0100

    task service init database

---
+++ tasks-service/tasks-service/src/test/java/pl/edu/agh/tai/tasksservice/TasksServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.tai.tasksservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
