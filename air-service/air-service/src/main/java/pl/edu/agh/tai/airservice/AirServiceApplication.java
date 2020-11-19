494c63ed86ba3c4f74770777840b75a6c3451f34
unknown <lukasz.jalowiecki@gmail.com>
2020-11-19 19:40:49 +0100

    air-service init

---
+++ air-service/air-service/src/main/java/pl/edu/agh/tai/airservice/AirServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.agh.tai.airservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirServiceApplication.class, args);
+	}
+
+}
