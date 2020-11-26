d36ff68094d3604cc80ff237e718af9eeeff6c73
unknown <lukasz.jalowiecki@gmail.com>
2020-11-27 00:14:51 +0100

    change routing for air-service

--- air-service/air-service/src/main/java/pl/edu/agh/tai/airservice/AirServiceApplication.java
+++ air-service/air-service/src/main/java/pl/edu/agh/tai/airservice/AirServiceApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
