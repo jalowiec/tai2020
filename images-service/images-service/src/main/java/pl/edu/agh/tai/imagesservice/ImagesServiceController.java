fdc455270b58e9a7b86cfc0567f4226913f2cc4f
unknown <lukasz.jalowiecki@gmail.com>
2021-02-06 19:30:00 +0100

    add token to image service

--- images-service/images-service/src/main/java/pl/edu/agh/tai/imagesservice/ImagesServiceController.java
+++ images-service/images-service/src/main/java/pl/edu/agh/tai/imagesservice/ImagesServiceController.java
@@ -6,13 +6,14 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequestMapping("/users/{id}")
 public class ImagesServiceController {
 
     @Autowired
@@ -24,7 +25,7 @@ public class ImagesServiceController {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
-    @GetMapping("/images/image")
+    @RequestMapping(method = RequestMethod.GET, value = "/image", produces= MediaType.APPLICATION_JSON_VALUE)
     @HystrixCommand(fallbackMethod = "getDefaultImage")
     public ImageDto getImage(){
         return ImageMapper.ImageToImageDto(getExternalImage());
